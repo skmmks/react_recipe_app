926621163baf5b5f710bb47d0093d107416fbb28
Steve Min <stevekmin@gmail.com>
2020-02-12 04:14:27 +0000

    title calories and image appear on dom from 3rd party API

--- src/App.js
+++ src/App.js
@@ -30,7 +30,11 @@ const App = () => {
         </button>
       </form>
       {recipes.map(recipe => (
-        <Recipe />
+        <Recipe
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories}
+          image={recipe.recipe.image}
+        />
       ))}
     </div>
   );
