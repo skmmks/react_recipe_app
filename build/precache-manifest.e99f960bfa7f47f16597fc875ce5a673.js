278cbac880dabd49f574a2f229a851d4b6b41e2c
Steve Min <stevekmin@gmail.com>
2020-02-12 00:14:04 +0000

    create app component create useEffect method to call get Recipe create getRecipe method for fetch call store response into json

---
+++ build/precache-manifest.e99f960bfa7f47f16597fc875ce5a673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7eeb59d80956ca468f39fa47ee9b05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eba01631a58beec5fd43",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1dc28d296ed4d65cca75",
+    "url": "/static/js/2.3663cc84.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.3663cc84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eba01631a58beec5fd43",
+    "url": "/static/js/main.3e0137a9.chunk.js"
+  },
+  {
+    "revision": "edef83dc7cfc091a7ced",
+    "url": "/static/js/runtime-main.3d54514f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const APP_ID = "e8756faa";
+  const APP_KEY = "b68b0b4cc20362d37e8ae3664007f672";
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = response.json();
+
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="searchForm">
+        <input type="text" className="searchBar" />
+        <button className="searchButton" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
-}
+};
 
 export default App;
